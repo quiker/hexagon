e202af0ada655d6a0ea3480e18e774063123dd7f
q3ker <q3ker.ak@gmail.com>
2013-11-10 23:22:53 +0400

    Save last completed level to preferences

--- Assets/Scripts/Util/SettingsContainer.cs
+++ Assets/Scripts/Util/SettingsContainer.cs
@@ -5,7 +5,7 @@ public class SettingsContainer {
 
 	public static bool GetMusicFlag()
 	{
-		return PlayerPrefs.GetInt("music_flag")!=0;
+		return PlayerPrefs.GetInt("music_flag", 1)!=0;
 	}
 	
 	public static void SetMusicFlag(bool flag)
@@ -16,7 +16,7 @@ public class SettingsContainer {
 	public static int GetLevelStars(int level)
 	{
 		if (level > 0 && level <= 30) {
-			return PlayerPrefs.GetInt("level"+level+"_stars");
+			return PlayerPrefs.GetInt("level"+level+"_stars", 0);
 		}
 		return 0;
 	}
@@ -24,7 +24,7 @@ public class SettingsContainer {
 	public static void SetLevelStars(int level, int stars)
 	{
 		if (level > 0 && level <= 30 && stars > 0 && stars <= 3) {
-			int currentStars = PlayerPrefs.GetInt("level"+level+"_stars");
+			int currentStars = PlayerPrefs.GetInt("level"+level+"_stars", 0);
 			if (stars > currentStars) {
 				PlayerPrefs.SetInt("level"+level+"_stars", stars);
 			}
@@ -34,7 +34,7 @@ public class SettingsContainer {
 	public static int GetLevelMaxScore(int level)
 	{
 		if (level > 0 && level <= 30) {
-			return PlayerPrefs.GetInt("level"+level+"_stars");
+			return PlayerPrefs.GetInt("level"+level+"_stars", 0);
 		}
 		return 0;
 	}
@@ -42,7 +42,7 @@ public class SettingsContainer {
 	public static void SetLevelMaxScore(int level, int score)
 	{
 		if (level > 0 && level <= 30) {
-			int currentMaxScore = PlayerPrefs.GetInt("level"+level+"_max_score");
+			int currentMaxScore = PlayerPrefs.GetInt("level"+level+"_max_score", 0);
 			if (score > currentMaxScore) {
 				PlayerPrefs.SetInt("level"+level+"_max_score", score);
 			}
@@ -52,7 +52,7 @@ public class SettingsContainer {
 	public static string GetScoreTableLabel(int row)
 	{
 		if (row >= 0 && row < 10) {
-			return PlayerPrefs.GetString("score_table_"+row+"_label");
+			return PlayerPrefs.GetString("score_table_"+row+"_label", 0);
 		}
 		return "";
 	}
@@ -60,8 +60,18 @@ public class SettingsContainer {
 	public static int GetScoreTableValue(int row)
 	{
 		if (row >= 0 && row < 10) {
-			return PlayerPrefs.GetInt("score_table_"+row+"_value");
+			return PlayerPrefs.GetInt("score_table_"+row+"_value", 0);
 		}
 		return 0;
+	}
+	
+	public static int GetLastCompletedLevel()
+	{
+		return PlayerPrefs.GetInt("last_completed_level", 0);
+	}
+	
+	public static void SetLastCompletedLevel(int level)
+	{
+		PlayerPrefs.SetInt("last_completed_level", level);
 	}
 }
